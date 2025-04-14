aecbff5f40ba6d2ec73f65a2605c8a4a29d11cfa
adamKHTML <95083105+adamKHTML@users.noreply.github.com>
2025-04-14 15:01:35 +0200

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import HeroSplit from '../components/sections/HeroSplit';
+import FeaturesTiles from '../components/sections/FeaturesTiles';
+import FeaturesTabs from '../components/sections/FeaturesTabs';
+import News from '../components/sections/News';
+import Pricing from '../components/sections/Pricing';
+import Cta from '../components/sections/Cta';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <HeroSplit hasBgColor invertColor />
+        <FeaturesTiles />
+        <FeaturesTabs topDivider bottomOuterDivider />
+        <News className="illustration-section-01" />
+        <Pricing hasBgColor pricingSlider />
+        <Cta hasBgColor invertColor split />
+      </React.Fragment>
+    );
+  }
+}
+
+export default Home;
